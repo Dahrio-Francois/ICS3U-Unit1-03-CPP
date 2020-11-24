999d976861b592aff5764c97af0b46e510cb5aec
Ubuntu <ubuntu@ip-172-31-39-151.ec2.internal>
2020-11-24 01:55:49 +0000

    added in hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+// copyright (c) 2019 St. Mother Teresa HS All rights reserved.
+//
+// Created by: Dahrio Francois
+// Created on: November 2020
+// This is the "Hello, World!" program, with proper style.
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+}
